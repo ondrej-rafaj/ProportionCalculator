e84ecfd20dc40b15824e56b9820b259044c37cbd
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-04-05 16:14:16 +0100

    calculator class and images + some language changes
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

---
+++ ProportionCalculator/ProportionCalculator/FTCalculatorKeyboardView.h
@@ -0,0 +1,13 @@
+//
+//  FTCalculatorKeyboardView.h
+//  ProportionCalculator
+//
+//  Created by Ondrej Rafaj on 03/04/2013.
+//  Copyright (c) 2013 Fuerte Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTCalculatorKeyboardView : UIView
+
+@end
---
+++ ProportionCalculator/ProportionCalculator/FTCalculatorKeyboardView.m
@@ -0,0 +1,32 @@
+//
+//  FTCalculatorKeyboardView.m
+//  ProportionCalculator
+//
+//  Created by Ondrej Rafaj on 03/04/2013.
+//  Copyright (c) 2013 Fuerte Innovations. All rights reserved.
+//
+
+#import "FTCalculatorKeyboardView.h"
+
+@implementation FTCalculatorKeyboardView
+
+- (id)init
+{
+    CGRect frame = CGRectMake(0, 0, 320, 200);      
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
